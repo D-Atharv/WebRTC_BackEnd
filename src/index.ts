9e336954ad6877d14b8d6f57b3512133688a913d
D-Atharv <atharvdewanganakd298suncity@gmail.com>
2024-08-31 00:13:56 +0530

    basic socket project setup done

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ app.get('/', (req: Request, res: Response) => {
     res.send('Hello from Express.js!');
 });
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
 
--- src/sockets/socket.io.ts
+++ src/sockets/socket.io.ts
@@ -2,7 +2,12 @@ import { Server as SocketIOServer } from 'socket.io';
 import http from 'http';
 
 export function initializeSocketIO(server: http.Server): SocketIOServer {
-    const io = new SocketIOServer(server);
+    const io = new SocketIOServer(server, {
+        cors: {
+            origin: "http://localhost:3000", // frontend URL
+            methods: ["GET", "POST"]
+        }
+    });
 
     io.on('connection', (socket) => {
         console.log('user/server connected');
